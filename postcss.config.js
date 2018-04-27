620a7150b0000dd32cc6ceb120d03754ec10878e
Leon.Xu <xhl@live.it>
2018-04-27 13:46:37 +0800

    `url()` support `containers/Home/logo.png`, `./logo.png`, `../logo.png`

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,9 @@
 module.exports = ({ file }) => ({
   plugins: {
     'postcss-import': { root: file.dirname },
-    'postcss-url': {},
+    'postcss-url': [
+      { filter: './**.*', url: asset => `./${asset.url}` }, // [relative path](https://git.io/vplP2)
+    ],
     // http://cssnext.io/usage, http://browserl.ist
     'postcss-cssnext': {
       browsers: ['last 2 version'],
