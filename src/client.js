2b1942c85dd91893e9a1688ceede0e9193649741
Kévin Berthommier <kevin.bertho@gmail.com>
2018-09-28 22:55:08 +0200

    fix hmr

--- src/client.js
+++ src/client.js
@@ -111,7 +111,7 @@ initSocket();
   if (module.hot) {
     module.hot.accept('./routes', () => {
       const nextRoutes = require('./routes');
-      hydrate(nextRoutes).catch(err => {
+      hydrate(nextRoutes.__esModule ? nextRoutes.default : nextRoutes).catch(err => {
         console.error('Error on routes reload:', err);
       });
     });
